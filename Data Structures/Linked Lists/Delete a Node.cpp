50d05f45e1bad87f5a7e56defd9c58d8a2f553bb
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2020-10-21 14:20:38 +0300

    Add files via upload

---
+++ Data Structures/Linked Lists/Delete a Node.cpp
@@ -0,0 +1,17 @@
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+        SinglyLinkedListNode* temp1 = head;
+        if(position == 0){
+            head = temp1->next;
+            delete temp1;
+        }
+        else{
+            for(int i = 1; i < position; i++){
+                temp1 = temp1->next;
+            }
+            SinglyLinkedListNode* temp2 = temp1->next;
+            temp1->next = temp2->next;
+            delete temp2;
+        }
+        return  head;
+
+}
