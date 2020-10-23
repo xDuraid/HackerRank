fd37d699d2be3bdc609a3ebef2d0caf229a071f2
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2020-10-23 18:45:24 +0300

    Update Print the Elements of a Linked List.cpp

--- Data Structures/Linked Lists/Print the Elements of a Linked List.cpp
+++ Data Structures/Linked Lists/Print the Elements of a Linked List.cpp
@@ -1,6 +1,17 @@
 //Hacker Rank - Data Structure Practice Problem
 //Print the Elements of a Linked List Solution
 
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
 void printLinkedList(SinglyLinkedListNode* head) {
     while (head != nullptr) {
         cout << head->data << endl;
