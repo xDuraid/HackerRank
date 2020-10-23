ac09528563c271f85d40c5ccdd00c05414734024
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2020-10-23 18:44:05 +0300

    Add files via upload

---
+++ Data Structures/Linked Lists/Compare two linked lists.cpp
@@ -0,0 +1,27 @@
+//Hacker Rank - Data Structure Practice Problem
+//Compare two linked lists Solution
+
+// Complete the compare_lists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+
+    while(head1 != nullptr && head2 != nullptr){
+        if(head1->data != head2->data)
+            return false;
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    if(head1 == nullptr && head2 == nullptr)
+        return true;
+    return false;
+}
