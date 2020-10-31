7402358de5971379128862697917c23435117fa9
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2020-10-31 14:07:49 +0200

    Add files via upload

---
+++ Data Structures/Linked Lists/Cycle Detection.cpp
@@ -0,0 +1,25 @@
+//Hacker Rank - Data Structure Practice Problem
+//Cycle Detection Solution
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool has_cycle(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* temp1 = head;
+    SinglyLinkedListNode* temp2 = head;
+    while(temp1 != nullptr && temp1->next != nullptr && temp2 != nullptr){
+        temp1 = temp1->next->next;
+        temp2 = temp2->next;
+        if(temp1 == temp2)
+            return true;
+    }
+    return false;
+}
